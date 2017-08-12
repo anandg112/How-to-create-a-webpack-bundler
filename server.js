ddc59d2370ccfb15fd071e0b25f60fd4d1dae9a2
anandg112 <anandg112@gmail.com>
2017-08-12 15:19:35 -0400

    added production enviroment to server

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const path = require('path');
 
 
-const port = 3050;
+const port = process.env.PORT || 3050;
 
 const app = express();
+
+//server routes....
+
 
 if(process.env.NODE_ENV !== 'production') {
   const webpackMiddleware = require('webpack-dev-middleware');
   const webpack = require('webpack');
   const webpackConfig = require('./webpack.config.js');
   app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
 }
 
 
